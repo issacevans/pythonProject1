82bd0163968d40675d81d1365c2ea34b163ae691
issacevans <bellanovember1st@gmail.com>
2023-06-07 15:19:34 +0800

    0607

--- fourth_code.py
+++ fourth_code.py
@@ -3,6 +3,7 @@ class OutPerson:
     def __init__(self):
         self.people_num = 0
         self.lis = []
+
     def new_lis(self, people_num):
         self.people_num = people_num
         for i in range(self.people_num):
@@ -15,28 +16,30 @@ class OutPerson:
         out_num = 0
         count = 1
         index = 0
-        while 1 == 1:
+        while out_num < (self.people_num - 1):
+            # index %= self.people_num
             if index == self.people_num:
                 index = 0
-            if out_num == (self.people_num - 1):
-                break
+            # if out_num == (self.people_num - 1):
+            #     break
             if self.lis[index] != 0:
                 index += 1
                 continue
-
             if count == 4:
                 count = 0
                 out_num += 1
                 self.lis[index] = out_num
             index += 1
             count += 1
-        #print(self.lis)
+        # print(self.lis)
+
     def __find_final_out(self):
-        for i in range(self.people_num):
-            if self.lis[i]==0:
-                print("最後留下來的是第"+str(i+1)+"位")
+        for i, x in enumerate(self.lis):
+            if x == 0:
+                print("最後留下來的是第" + str(i + 1) + "位")
                 break
 
+
 if __name__ == '__main__':
-    outperson = OutPerson()
-    outperson.new_lis(10)
+    num_p = OutPerson()
+    num_p.new_lis(1000000)
